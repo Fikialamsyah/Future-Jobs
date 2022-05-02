bf5fed35a8962e06da8f7ae0d7584b1c30da977d
astro <fikialamsyah16@gmail.com>
2022-05-02 22:41:55 +0700

    main: add sign in page

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,12 +1,171 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:future_jobs/pages/signup_page.dart';
+import 'package:future_jobs/themes.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({ Key? key }) : super(key: key);
 
   @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+
+  bool isEmailValid = true;
+  TextEditingController emailController = TextEditingController(text: '');
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30.0, left: 24, right: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Sign In',
+                  style: greyTextColor.copyWith(
+                    fontSize: 16
+                  ),
+                ),
+                Text(
+                  'Build Your Career',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 20, 
+                    fontWeight: bold
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Center(
+                  child: Image.asset(
+                    'assets/image_sign_in.png',
+                    width: 261,
+                    height: 240,
+                  ),
+                ),
+                const SizedBox(
+                  height: 49,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                   Text(
+                      'Email Address',
+                      style: greyTextColor.copyWith(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      onChanged: (value) {
+                        print(value);
+                        bool isValid = EmailValidator.validate(value);
+                        print(isValid);
+                        if (isValid) {
+                          setState(() {
+                            isEmailValid = true;
+                          });
+                        } else {
+                          setState(() {
+                            isEmailValid = false;
+                          });
+                        }
+                      },
+                      decoration: InputDecoration(
+                          fillColor: inputFieldColor,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color:
+                                      isEmailValid ? primaryColor : redColor),
+                              borderRadius: BorderRadius.circular(100))),
+                      style: TextStyle(
+                          color: isEmailValid ? primaryColor : redColor),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Password',
+                      style: greyTextColor.copyWith(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: inputFieldColor,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: primaryColor),
+                              borderRadius: BorderRadius.circular(100))),
+                      style: TextStyle(
+                          color: isEmailValid ? primaryColor : redColor),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Center(
+                      child: Container(
+                        width: 400,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: (){
+
+                          },
+                          child: Text(
+                            'Sign In',
+                            style: whiteTextColor,
+                          ),
+                          style: TextButton.styleFrom(
+                            backgroundColor: primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(66)
+                            )
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 40),
+                          child: Text(
+                            'Create New Account',
+                            style: greyTextColor.copyWith(
+                              fontSize: 14,
+                              fontWeight: light
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
